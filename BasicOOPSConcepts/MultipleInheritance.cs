24c64928b8aec12ffab28692a82254b3f2dfa258
Anand Muragayya Sankrattimath <anand.sankrattimath@kalpitatechnologies.com>
2022-08-04 14:18:53 +0530

    Class,object,Encapsualation,MultipleInheritance are uploaded

---
+++ BasicOOPSConcepts/MultipleInheritance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOOPSConcepts
+{
+     class Parent1
+    {
+        public void eat()
+        {
+            Console.WriteLine("In Parent1 class");
+        }
+    }
+
+    class Parent2
+    {
+        public void eat()
+        {
+            Console.WriteLine("In Parent2 class");
+        }
+    }
+
+    class Child : Parent1, Parent2 // Compile time error i.e Child class does not have the multple base classes.
+    {
+
+    }
+}
---
+++ BasicOOPSConcepts/Program.cs
@@ -0,0 +1,48 @@
+﻿using BasicOOPSConcepts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOOPSConcepts
+{
+    public class Citizen  // Creating class
+    {
+        int id;
+        String name;
+        String address;
+        Boolean isEmployee;
+
+        public void saveDetails( int id,String name,String address,Boolean isEmployee) // method/function with parameters
+        {
+            Console.WriteLine("saveDetails() started");
+            this.id = id;
+            this.name = name;
+            this.address = address;
+            this.isEmployee = isEmployee; // value initialization.
+            Console.WriteLine("Successfully details saved...");
+        }
+
+       
+
+        public void getDetails() // method/function without parameter.
+        {
+            Console.WriteLine("getDetails() started");
+            Console.WriteLine("Name of citizen is {0}", name);
+            Console.WriteLine("Address of citizen is "+address); 
+            Console.WriteLine("Details were displayed...");
+        }
+
+    } 
+}
+class ObjectCreation
+{
+    static void Main()
+    {
+        Citizen manish = new Citizen();
+        manish.saveDetails(111, "Manish Mullur", "Bagalkot", true);
+        manish.getDetails();
+        Console.ReadLine();
+    }
+}
